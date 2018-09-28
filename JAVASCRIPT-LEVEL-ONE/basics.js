c5ff6576ee283f7715e003a4c4ea1f423af696b9
Guilherme Freitas <guilherme@vert-capital.com>
2018-09-28 18:25:10 -0300

    adiciona o primeiro arquivo do módulo de JavaScript;

---
+++ JAVASCRIPT-LEVEL-ONE/basics.js
@@ -0,0 +1,83 @@
+alert("Hello, world.");
+
+// Isto é um comentário
+
+
+// Basic data types
+
+10;
+20.2;
+-13.4;
+
+
+// Strings
+
+"Hello world";
+"10";
+
+
+// Booleans
+
+true;
+false;
+
+
+// Undefined and null
+
+undefined;
+
+null;
+
+
+// Basic calculator:
+
+2+2;
+5-1;
+3*2;
+10/2;
+2/5;
+2 ** 4;
+15 % 14;
+6 % 2;
+6 % 4;
+
+
+// Strings
+
+"Django is super cool";
+"Django" + "is super cool";
+"Django " + "is super cool";
+
+"django".length;
+
+"x x".length; // whitespaces counts
+
+"starts \n a new line";
+
+"hello \t give me a new tab";
+
+"hello \"quotes\"";
+
+
+// Indexing stars at zero:
+
+"hello"[0];
+
+
+// Variables
+
+var bankAccout = 100;
+var deposit = 50;
+var total = bankAccount + deposit;
+total;
+
+var greeting = "Welcome back: ";
+var name = "Gui";
+alert(greeting + name);
+
+var myVariable;
+var bonus = null;
+
+console.log("Hey! I'm poshto and I'm in the console");
+
+console.log(prompt("Digite algo: "))
