c3d42f15ae7d5f1d62280d64ea126da80cf527aa
Guilherme Freitas <guilherme@vert-capital.com>
2018-10-01 18:16:59 -0300

    Adiciona o arquivo da aula sobre operadores

---
+++ JAVASCRIPT-LEVEL-ONE/operators.js
@@ -0,0 +1,42 @@
+2 == 2 // true
+
+"user" == "user" // true
+
+"2" == 2 // true
+
+"2" === 2 // false
+
+5 !== "5" // true
+
+5 != "5" // false 
+
+// Operadores Booleanos
+
+true == 1 // true
+true === 1 // false
+
+false == 0
+false === 0
+
+null == undefined // true
+null === undefined // false
+
+NaN == NaN // false
+
+// Operadores loǵicos
+
+1 === 1 && 2 === 2 // true
+1 === 1 && 2 === 2 && 1 === 2 // false
+
+//or
+
+1 === 2 || 1 === 1 // true
+
+//not
+
+(1 === 1) // true
+!(1 === 1) // false
+
+!!(1 === 1) // true
+
+!!!!!(1 === 1) // false
