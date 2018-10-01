02e595a660de8f2bd05b7b9c3cb7e5e804b144d3
Guilherme Freitas <guilherme@vert-capital.com>
2018-10-01 17:33:06 -0300

    Adiciona o arquivo de exercício já com o exercício pronto.

---
+++ JAVASCRIPT-LEVEL-ONE/exercise.js
@@ -0,0 +1,11 @@
+// Pedido
+var weight_lbs = prompt("Qual é o peso (em lbs)?");
+
+// Conversão de pounds em kg
+var weight_kg = (weight_lbs * 0.45359237);
+
+// Print do resultado
+alert("Resultado da conversão: " +weight_kg);
+
+// Console.log
+console.log("Conversão finalizada com sucesso.")
