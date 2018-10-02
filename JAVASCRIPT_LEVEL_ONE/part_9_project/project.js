cff4b9a90d6f6a14f9b37058e09847231be88923
Guilherme Freitas <guilherme@vert-capital.com>
2018-10-02 17:27:02 -0300

    Corrige o exercício. Agora ele não tem mais bugs.

--- JAVASCRIPT_LEVEL_ONE/part_9_project/project.js
+++ JAVASCRIPT_LEVEL_ONE/part_9_project/project.js
@@ -6,39 +6,42 @@ var height = prompt("How tall are you? (in centimeters)")
 var petName = prompt("What's your pet's name?")
 
 // Main boolean test:
-spy = null;
+nameSpy = null;
+ageSpy = null;
+heightSpy = null;
+petNameSpy = null;
 
 // name and lastName:
 if (firstName[0] === lastName[0]) { // Se a primeira letra de nome e sobrenome forem iguais
-    spy = true;
+    nameSpy = true;
 } else {
-    spy = false;
+    nameSpy = false;
 }
 
 // age:
 if (age > 20 && age < 30) { // se a idade estiver entre 21 e 29
-    spy = true;
+    ageSpy = true;
 } else {
-    spy = false;
+    ageSpy = false;
 }
 
 // height:
 if (height >= 170) { // se a altura for maior que 169 centímetros
-    spy = true;
+    heightSpy = true;
 } else {
-    spy = false;
+    heightSpy = false;
 }
 
 // petName:
 if (petName.charAt(petName.length - 1) === "y") { // se a última letra do petName for igual a "y"
-    spy = true;
+    petNameSpy = true;
 } else {
-    spy = false;
+    petNameSpy = false;
 }
 
-// Final secret prompt:
-if (spy == false) {
-    console.log("There's nothing here. Come back later.")
+// Secret prompt:
+if (nameSpy && ageSpy && heightSpy && petNameSpy) {
+    console.log("Welcome aboard, fellow friend. Please take a seat.")
 } else {
-    console.log("Welcome aboard, fellow spy. Please take a seat.")
+    console.log("There's nothing here. Come back later.")
 }
