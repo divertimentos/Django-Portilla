caaf3bf73a0fb47c761a57ac943859e1465aada6
Guilherme Freitas <guilherme@vert-capital.com>
2018-10-02 10:38:03 -0300

    Adiciona um arquivo com exemplos de estruturas de controle básicas

---
+++ JAVASCRIPT-LEVEL-ONE/control_flow.js
@@ -0,0 +1,23 @@
+// IF Statement
+
+if (condition) {
+    // Execute some code
+}
+
+// Else Statement
+
+if (condition) {
+    // Execute some code
+}else{
+    // Execute some other code
+}
+
+// Else if Statement
+
+if (condition one) {
+    // Execute some code
+} else if {
+    // Execute some other code
+} else {
+    // Execute some backup code
+}
