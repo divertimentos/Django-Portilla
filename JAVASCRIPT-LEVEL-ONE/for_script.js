582f2f89a0dcca75e73c8203479147910e514515
Guilherme Freitas <guilherme@vert-capital.com>
2018-10-02 12:21:31 -0300

    Adiciona os arquivos da aula sobre for loops.

---
+++ JAVASCRIPT-LEVEL-ONE/for_script.js
@@ -0,0 +1,17 @@
+// For Loops
+
+for (var i = 0; i <= 5; i++) {
+    console.log("Number is: " + i);
+}
+
+var word = "Pneumoultramicroscopicossilicovulcanoconiose"
+
+for (var i = 0; i <  word.length; i++) {
+    console.log(word[i]);
+}
+
+var word = "ABABABABAB"
+
+for (var i = 0; i < word.length; i+= 2) {
+    console.log(word[i]);
+}
