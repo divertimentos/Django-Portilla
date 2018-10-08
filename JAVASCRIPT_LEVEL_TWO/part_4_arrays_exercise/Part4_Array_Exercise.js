cf0d1bb2d617bede0daf501d5e61ecb5bc578bc4
Guilherme Freitas <guilherme@vert-capital.com>
2018-10-08 13:36:10 -0300

    3 funções feitas no exercício de array (parte 4)

--- JAVASCRIPT_LEVEL_TWO/part_4_arrays_exercise/Part4_Array_Exercise.js
+++ JAVASCRIPT_LEVEL_TWO/part_4_arrays_exercise/Part4_Array_Exercise.js
@@ -12,6 +12,10 @@ var roster = []
 // Create a function called addNew that takes in a name
 // and uses .push to add a new student to the array
 
+function addNew(name) {
+    roster.push(name);
+    return name;
+}
 
 // REMOVE STUDENT
 
@@ -21,12 +25,26 @@ var roster = []
 // HINT: http://stackoverflow.com/questions/5767325/how-to-remove-a-particular-element-from-an-array-in-javascript
 //
 
+function remove(name) {
+    var index = roster.indexOf(name);
+    roster.splice(index, 1);
+}
+
 // DISPLAY ROSTER
 
 // Create a function called display that prints out the orster to the console.
 
+function display() {
+    return console.log(roster);
+}
 
 // Start by asking if they want to use the web app
+
+var question = prompt("Would you like to start the roster web app? (y/n)")
+
+if (question[0] === "n") {
+    alert("você escolheu não")
+}
 
 // Now create a while loop that keeps asking for an action (add,remove, display or quit)
 // Use if and else if statements to execute the correct function for each command.
