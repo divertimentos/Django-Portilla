fdc5fc31642431058f4a4f4bebef0465db664af3
guiemi <guiemi.teixeira@gmail.com>
2019-02-04 22:15:26 -0200

    Insere mais exemplos de objetos em JavaScript

--- JAVASCRIPT_LEVEL_TWO/part_5_objects/divertimentos.js
+++ JAVASCRIPT_LEVEL_TWO/part_5_objects/divertimentos.js
@@ -6,3 +6,28 @@ var carInfo = {
         alert("We've got a car here!")
     }
 };
+
+// This example
+var myObj = {
+    prop: 37,
+    reportProp: function(){
+        return this.prop;
+    }
+};
+console.log(myObj.reportProp()); // logs 37
+
+// Another this example
+var simple = {
+	prop: "Hello",
+	myMethod: function(){
+		console.log("The myMethod was called")
+	}
+}
+
+// Even another this example
+var myObj = {
+	name: "Jose",
+	greet: function(){
+        console.log("Hello" + this.name)
+    }
+}
