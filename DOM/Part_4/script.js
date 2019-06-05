9be7f648c93b32b08e94cdc04fed9c2c007fa0e8
Guilherme Teixeira <guiemi.teixeira@gmail.com>
2019-06-05 18:25:17 -0300

    WIP: escrevendo um jogo da velha usando eventListener

---
+++ DOM/Part_4/script.js
@@ -0,0 +1,15 @@
+/* var tableLevel = document.querySelector(".table")
+var bodyLevel = tableLevel.querySelector("#tableBody")
+var rowLevel = bodyLevel.querySelector(".rowOne")
+
+rowLevel.addEventListener('mouseover', function(){
+    this.style.color = "red"
+})
+ */
+
+var firstRow = document.querySelectorAll(".table")
+
+
+firstRow.addEventListener('mouseover', function(){
+    firstRow.style.color = "red"
+})
