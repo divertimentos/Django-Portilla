2c2c972f54a9b1537244de2c30f59bdefa36e4ea
guiemi <guiemi.teixeira@gmail.com>
2019-06-04 21:07:18 -0300

    Renomeia a pasta DOM e reestrutura suas pastas-filhas

--- DJANGO_LEVEL_ONE/first_project/first_app/views.py
+++ DJANGO_LEVEL_ONE/first_project/first_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from first_app.models import Topic, Webpage, AccessRecord
+from models import Topic, Webpage, AccessRecord
 
 
 def index(request):
